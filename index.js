887318b93edc6e98fa4a132d2c3545827a2be82c
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-05-24 16:13:01 +0200

    Select necessary elements from HTML

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import posts from './posts.js'
+
+const userAvatarEl = document.querySelector('.user-avatar')
+const userNameEL = document.querySelector('.user-name')
+const postLocationEl = document.querySelector('.post-location')
+const postImageEl = document.querySelector('.post-image')
+const postLikesEl = document.querySelector('.post-likes')
+const commentUserNameEl = document.querySelector('.comment-user-name')
+const commentEl = document.querySelector('.comment')
