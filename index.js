1b792f186e95256e4457b87929d91c7aae84240e
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-05-24 18:25:40 +0200

    Refactor classes and add function to increment the likes onClick

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ posts.forEach((post) => {
   postElement.innerHTML = `
       <div class="post-header">
         <div class="avatar-container">
-          <img src="${post.avatar}" alt="Avatar Image" class="avatar user-avatar" />
+          <img src="${post.avatar}" alt="Avatar Image" class="avatar" />
         </div>
         <div>
-          <p class="bold-text user-name">${post.name}</p>
-          <p class="post-location">${post.location}</p>
+          <p class="bold-text">${post.name}</p>
+          <p>${post.location}</p>
         </div>
       </div>
       <img src="${post.post}" alt="Post Image" class="post-image" />
-      <div class="post-footer">
+      <div>
         <div class="post-interactions">
           <div class="icons-container">
-            <img src="images/icon-heart.png" alt="Like Icon" class="icons" />
+            <img src="images/icon-heart.png" alt="Like Icon" class="icons like-icon" />
             <img src="images/icon-comment.png" alt="Comment Icon" class="icons" />
             <img src="images/icon-dm.png" alt="DM Icon" class="icons" />
           </div>
           <p class="bold-text post-likes">${post.likes} likes</p>
-          <p class="comment">
+          <p>
             <span class="bold-text comment-user-name">${post.username}</span> ${post.comment}
           </p>
         </div>
@@ -31,4 +31,14 @@ posts.forEach((post) => {
     `
 
   container.appendChild(postElement)
+})
+
+const likeIcons = document.querySelectorAll('.like-icon')
+const postLikes = document.querySelectorAll('.post-likes')
+
+likeIcons.forEach((likeIcon, index) => {
+  likeIcon.addEventListener('click', () => {
+    postLikes[index].textContent =
+      parseInt(postLikes[index].textContent) + 1 + ' likes'
+  })
 })
